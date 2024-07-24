e2a554f105ec8a4dd0da20a95f91be339dafe77c
sonu kumar <sonu.kumar.28.2@mountblue.tech>
2024-07-24 15:55:49 +0530

    code reformatted

--- src/main/java/com/doowup/page_objects/BasicProfilePage.java
+++ src/main/java/com/doowup/page_objects/BasicProfilePage.java
@@ -7,7 +7,6 @@ import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.FindBy;
 import org.openqa.selenium.support.PageFactory;
 import org.openqa.selenium.support.ui.ExpectedConditions;
-import org.openqa.selenium.support.ui.Select;
 import org.openqa.selenium.support.ui.WebDriverWait;
 
 import java.time.Duration;
@@ -79,7 +78,7 @@ public class BasicProfilePage extends BaseClass {
         submitUsageBtn.click();
     }
 
-    public void submitdetail(){
+    public void submitdetail() {
         try {
             selectApartment.click();
             WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(2));
